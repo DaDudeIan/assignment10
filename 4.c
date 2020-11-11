5d49e3df28a08f5f585dd6f1b78146c911c0ed32
DaDudeIan <iandahloliver@hotmail.com>
2020-11-11 12:16:19 +0100

    Slight update

--- 4.c
+++ 4.c
@@ -1,5 +1,46 @@
+/*
+ * The nodes (and leafs) in a binary tree are ordered such that,
+ * the left child of a node is smaller or equal than the item of the node and the right
+ * child is larger.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct tree_node {
+    int value;
+    tree_node* lchild;
+    tree_node* rchild;
+} tree_node;
+
+tree_node* make_node(int x) { //Needs more parameters
+    ;
+}
+
+void Insert(int x, tree_node* t) {
+    ;
+}
+
+void Remove(int x, tree_node* t) {
+    ;
+}
+
+bool Contains(int x, tree_node* t) {
+    ;
+}
+
+tree_node* Initialize(tree_node* t) {
+    ;
+}
+
+bool Full(tree_node* t) {
+    ;
+}
+
+bool Empty(tree_node* t) {
+    ;
+}
 
 int main(void) {
 
